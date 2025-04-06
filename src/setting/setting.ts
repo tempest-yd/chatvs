3f9957f184852c3696cba2aabe877de639dc144b
wangyida <530705067@qq.com>
2025-04-06 19:00:29 +0800

    settint 修复

--- src/setting/setting.ts
+++ src/setting/setting.ts
@@ -3,7 +3,7 @@ import { commands, ExtensionContext } from "vscode";
 export const registerCreateSetting = (context: ExtensionContext) => {
   context.subscriptions.push(
     commands.registerCommand("CodeToolBox.openSetting", () => {
-      commands.executeCommand("workbench.action.openSettings", "ChatVS");
+      commands.executeCommand("workbench.action.openSettings", "ai");
     }),
   );
 };
